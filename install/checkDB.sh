68626124a06002fb1b8aa8bfaafe09a482261962
Aaron Wilkinson <aaronwilkinson@gmail.com>
2017-10-16 10:29:27 -0500

    Initial commit

---
+++ install/checkDB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+				cd /usr/local/mysql/bin
+				./mysql -u root -pR4t*cyng2b1 --execute="SHOW Databases" mysql
+				
---
+++ install/createDB.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+				cd /usr/local/mysql/bin
+				./mysql -uroot -pR4t*cyng2b1 -e "CREATE DATABASE learncfinaweek"
+				
