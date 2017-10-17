44fd465e421065c737c069713f634d2c8f0ae552
Aaron Wilkinson <aaronwilkinson@gmail.com>
2017-10-17 14:53:55 -0500

    removing solution folders

--- install/checkDB.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-				cd /usr/local/mysql/bin
-				./mysql -u root -pR4t*cyng2b1 --execute="SHOW Databases" mysql
-				
--- install/createDB.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-				cd /usr/local/mysql/bin
-				./mysql -uroot -pR4t*cyng2b1 -e "CREATE DATABASE learncfinaweek"
-				
